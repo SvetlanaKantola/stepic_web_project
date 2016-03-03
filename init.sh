529d27b2a231db99e694b9da0818d16047383616
Svetlana Kantola <kant_2017@mail.ru>
2016-03-03 14:13:24 +0300

    test2

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo rm /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo /etc/gunicorn.d/wqunicorn -b '0.0.0.0:8080' hello:app
 sudo /etc/init.d/nginx restart
