af8fbd4ef29b26adf59ea4df1d5328f39ab9e1ab
Svetlana Kantola <kant_2017@mail.ru>
2016-03-03 17:04:35 +0300

    test2-2

--- hello.py
+++ hello.py
@@ -1,6 +1,17 @@
 def wsgi_app(environ,start_response):
+try:
 	status = '200 OK'
-	headers= [('Content-Type','text\plain')]
+	headers= [('Content-Type','text/plain')]
+#      body = []
+#	for key,value in environ.items90:
+#         body.append('%s=%r'%(key,value))
+#
 	body='\r\n'.join(environ['QUERY_STRING'].split('&'))	
 	start_response(status,headers)
 	return [body]
+except:
+	status = "500 Errorer"
+	headers= [('Content-Type','text/plain')]
+	start_response(status,headers,sys.exc_info())
+	return ["My Error"]
+	
--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
-sudo /etc/gunicorn.d/wqunicorn -b '0.0.0.0:8080' hello:app
+#sudo /etc/gunicorn.d/wqunicorn -c '0.0.0.0:8080' hello:app
+sudo /etc/gunicorn.d/gunicorn -c /home/box/web/etc/gunicorn.conf
 sudo /etc/init.d/nginx restart
