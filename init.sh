71ca8beb5451f334cf560649f22bbb41eba84bb9
Svetlana Kantola <kant_2017@mail.ru>
2016-03-03 19:42:02 +0300

    3-2

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 #sudo /etc/gunicorn.d/wqunicorn -c '0.0.0.0:8080' hello:app
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf --debug -D hello:wsgi_app
+#sudo /etc/init.d/gunicorn restart -c /home/box/web/etc/gunicorn.conf --debug -D hello:wsgi_app
+sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/nginx restart
