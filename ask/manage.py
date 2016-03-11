746b0237c10ed08d0020c542345067080953a054
Svetlana Kantola <kant_2017@mail.ru>
2016-03-11 12:08:20 +0300

    2-1-3

--- ask/manage.py
+++ ask/manage.py
@@ -2,7 +2,9 @@
 import os
 import sys
 
-if __name__=="--main__":
-	os.environ.setdefault("DJANGO_SETTINGS_MODULE","ask.settings")
-	from django.core.management import execute_from_comand_line
-	execute_from_comand_line(sys.argv)
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ask.settings")
+    
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
