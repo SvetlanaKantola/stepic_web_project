2c3cfc55a0048622fd964a8873090e5c23162eb6
Svetlana Kantola <kant_2017@mail.ru>
2016-03-11 19:52:42 +0300

    2-1-9

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
+#def test (environ,start_response):
+#	status = '200 OK'
+#	headers= [('Content-type','text/plain')]
+#	start_response(status,headers)
+#	body="\r\n".join(environ['QUERY_STRING'].split('&'))	
+#	return [body]
