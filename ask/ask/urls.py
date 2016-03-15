96b97c9bf1346ec7a03343584ca5852c68dfcae6
Svetlana Kantola <kant_2017@mail.ru>
2016-03-15 10:32:48 +0300

    2-1-14

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -16,6 +16,14 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/', 'qa.views.test'),
+    url(r'^signup/', 'qa.views.test'),
+    url(r'^question/', 'qa.views.test'),
+    url(r'^questions/\d+/.*$', 'qa.views.test'),
+    url(r'^ask/', 'qa.views.test'),
+    url(r'^popular/', 'qa.views.test'),
+    url(r'^new/', 'qa.views.test'),
 ]
