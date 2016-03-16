c83a1a40794b0536822e2b5b76cd1058ebd3dbeb
Svetlana Kantola <kant_2017@mail.ru>
2016-03-16 14:29:23 +0300

    2-1-21

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -22,4 +22,12 @@ urlpatterns = [
 #    url(r'^$', include('ask.qa.urls')),
     url(r'^', include('qa.urls')),
     url(r'^admin/', admin.site.urls),
+   url(r'^$', 'qa.views.test', name='index'),
+   url(r'^login/', 'qa.views.test', name='login'),
+   url(r'^signup/', 'qa.views.test', name='signup'),
+   url(r'^question/(?P<id>[0-9]+)/$', 'qa.views.test', name='question'),
+   url(r'^ask/', 'qa.views.test', name='ask'),
+   url(r'^popular/', 'qa.views.test', name='popular'),   
+   url(r'^new/', 'qa.views.test', name='new'),  
+
 ]
