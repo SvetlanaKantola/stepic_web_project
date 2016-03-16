d426ec3341e61c69d0a72b273189e4afac1b6b10
Svetlana Kantola <kant_2017@mail.ru>
2016-03-16 16:57:16 +0300

    2-1-23

--- init.sh
+++ init.sh
@@ -3,7 +3,8 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/gunicorn.py /etc/gunicorn.d/ask.py
 cd /home/box/web/ask/
 sudo /etc/init.d/nginx restart
-#sudo pip install --upgrade django
+#sudo pip install --upgrade gunicorn
+sudo pip install --upgrade django
 sudo rm /home/box/web/ask/la.log
 sudo rm /home/box/web/ask/err.log
 sudo gunicorn --bind 0.0.0.0:8000 --access-logfile /home/box/web/ask/la.log --error-logfile /home/box/web/ask/err.log ask.wsgi:application &
