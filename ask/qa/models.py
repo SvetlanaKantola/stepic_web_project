922455a9dc869a57fb4b1c19b6d4b5fbc7c09088
Svetlana Kantola <kant_2017@mail.ru>
2016-03-16 18:33:29 +0300

    2-5-1

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,5 +1,31 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+	title = models.CharField(max_length=100)
+	text = models.CharField(max_length=255)
+	added_at = models.DateTimeField(auto_now_add=True)
+	rating = models.IntegerField(default=1)
+	author =  models.CharField(max_length=20)
+	likes = models.ForeignKey(User,default=1)
+
+	def __unicode__(self) :
+		return self.title
+
+	def get_url(self) :
+		return reverse('question', kwargs={'id': self.id})
+
+
+class Answer(models.Model):
+	text = models.CharField(max_length=100)
+	added_at =  models.DateTimeField(auto_now_add=True)
+	question = models.ForeignKey(Question)
+	author = models.CharField(max_length=20)
+	
+	def __unicode__(self) :
+		return self.author + self.added_at + self.text
+
+
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,49 @@
-#from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_GET
+from django. core. paginator import Paginator
+from django.http import HttpResponse, Http404 
 
-from django.http import HttpResponse 
+from qa.models import Question, Answer
+
+@require_GET
+def main(request, *args, **kwargs):
+    try:    
+	questions = Question.objects.order_by('-id')
+    except Question.DoesNotExist:
+	raise Http404   		
+    page = request.GET.get('page', 1)    
+    paginator = Paginator(questions, 10)
+    paginator.baseurl = '/?page='
+    page = paginator.page(page)
+    return render(request,'questions.html',{
+	'questions': page.object_list,
+	'paginator': paginator, 'page': page,
+	})
+
+@require_GET
+def populars(request, *args, **kwargs):
+    try:    
+	populars = Question.objects.order_by('-rating')
+    except Question.DoesNotExist:
+	raise Http404   		
+    page = request.GET.get('page', 1)    
+    paginator = Paginator(questions, 10)
+    paginator.baseurl = '/popular/?page='
+    page = paginator.page(page)
+    return render(request,'questions.html',{
+	'questions': page.object_list,
+	'paginator': paginator, 'page': page,
+	})
+
+@require_GET
+def question(request, *args, **kwargs):
+    id = kwargs['id']
+    question = get_object_or_404(Question, id=id)		
+    answers = Answer.objects.filter(question=question)
+    return render(request,'question.html',{
+	'question': question,
+	'answers': answers,
+	})
+
 def test(request, *args, **kwargs):
     return HttpResponse('200 OK')
