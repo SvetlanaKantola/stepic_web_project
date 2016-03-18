23ddf75556349b10d8c1000c6c51291207541bbf
Svetlana Kantola <kant_2017@mail.ru>
2016-03-18 20:06:48 +0300

    2-5-17

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -20,27 +20,27 @@ class Question(models.Model):
 	text = models.CharField(max_length=255, null=True)
 	added_at = models.DateTimeField(auto_now_add=True, null=True)
 	rating = models.IntegerField(default=1)
-#	author =  models.CharField(max_length=20, null=True)
-#	likes = models.CharField(max_length=1000, null=True)
-	author =  models.CharField(max_length=20)
-	likes = models.ForeignKey(User,default=1)
+#	author =  models.CharField(max_length=20)
+#	likes = models.ForeignKey(User,default=1)
+	author = models.ForeignKey(User, null=True)
+	likes = models.CharField(max_length=1000, null=True)
 	
 	def __unicode__(self) :
 		return self.title
 
-	def get_url(self) :
-		return reverse('question', kwargs={'id': self.id})
+#	def get_url(self) :
+#		return reverse('question', kwargs={'id': self.id})
 
 
 class Answer(models.Model):
 	text = models.CharField(max_length=100, null=True)
 	added_at =  models.DateTimeField(auto_now_add=True, null=True)
-#	question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
-#	author = models.CharField(max_length=20, null=True)
-	question = models.ForeignKey(Question)
-	author = models.CharField(max_length=20)
+#	question = models.ForeignKey(Question)
+#	author = models.CharField(max_length=20)
+	question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+	author= models.ForeignKey(User, null=True)
 	
-	def __unicode__(self) :
-		return self.author + self.added_at + self.text
+#	def __unicode__(self) :
+#		return self.author + self.added_at + self.text
 
 
